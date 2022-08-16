5629c0b7d81039b31798362561809628542558c1
Alec Helmturner <git@alecvision.com>
2022-08-16 12:56:33 -0500

    fix prisma schema location bug

--- prisma/src/index.ts
+++
@@ -1,2 +0,0 @@
-import type * as zj from "zod-to-json-schema";
-import * as Models from "./zod";
--- unified-plugin-ts/src/index.ts
+++ unified-plugin-ts/src/index.ts
@@ -1,6 +1,14 @@
 import { Plugin, unified } from "unified";
 import { visit } from "unist-util-visit";
+import {Root} from 'mdast'
 
-const myPlugin: Plugin = () => {
+const myPlugin: Plugin<[], Root> = () => {
+    return (tree, file) => {
+        if (!tree !== 'mdast') {
+            throw new Error()
+        }
+        visit(tree, 'paragraph', (s, s) => {
 
+        })
+    }
 }
